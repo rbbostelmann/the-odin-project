e670c889f975dc6a8ddc66a2a342fe2d0f2e4fb3
rbbostelmann <rodrigobostelmann@gmail.com>
2022-05-18 19:01:13 -0300

    Decryption code added

--- ruby/01-Caesar Cipher/scipt.rb
+++ ruby/01-Caesar Cipher/scipt.rb
@@ -20,5 +20,26 @@ def translate(message, shift, result = "")
     result    
 end
 
-puts translate("b4anana", 16)
-puts translate("Ab", 1)
+puts translate("banana", 32)
+
+def decrypt(message, shift, result = "")
+    #Breaks down the cripted message in characters and take each character's ASCII value.
+    message.each_char do |char| 
+        #If the ASCII value is less than 91, we treat them as a base 'a' (65), otherwise we treat them as a base 'A' (97).
+        base = char.ord < 91 ? 65 : 97
+        #Lower case || Upper case
+        if char.ord.between?(65, 90) || char.ord.between?(97, 122)
+            #Math black magic happening "in reverse":
+            rotation = (((char.ord - base) - shift) % 26) + base
+            #Converts the new ASCII to a character and appends it to the result.
+            result += rotation.chr
+        else
+            #Keeps symbols, numbers, and whatnot.
+            result += char
+        end
+    end
+    #Returns the final decrypted string. 
+    result    
+end
+
+puts decrypt("hgtgtg", 32)
